9c1d5b2e24f0c4eb8b7a5ba0efbbdbf3ff644bdf
iamakkar <52326103+iamakkar@users.noreply.github.com>
2020-10-06 14:04:23 +0530

    password

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Email from "@material-ui/icons/Email";
 import Lock from "@material-ui/icons/Lock";
+import Eye from "@material-ui/icons/VisibilitySharp";
+import Eyecut from "@material-ui/icons/VisibilityOffSharp";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="wrapper">
-          <h1>Sign In</h1>
-          <div className="con-input">
-            <input placeholder="Email" type="text" />
-            <i className="icon">
-              <Email />
-            </i>
-            <div className="bg"></div>
-          </div>
-          <div className="con-input">
-            <input placeholder="Password" type="text" />
-            <i className="icon">
-              <Lock />
-            </i>
-            <div className="bg"></div>
-          </div>
-          <button className="btn">Log In</button>
-          <span>or</span>
-          <div className="afteror">
-            <a href="https://hacktoberfest.digitalocean.com" className="new">
-              Create an account
-            </a>
-            <div className="seperator"></div>
-            <a href="https://hacktoberfest.digitalocean.com" className="new">
-              Forgot password?
-            </a>
-          </div>
+function App() {
+  const [visible, setVisible] = useState(false);
+
+  function PasswordShow() {
+    if (!visible) setVisible(true);
+    else {
+      setVisible(false);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <h1>Sign In</h1>
+        <div className="con-input">
+          <input placeholder="Email" type="text" />
+          <i className="icon">
+            <Email />
+          </i>
+          <div className="bg"></div>
+        </div>
+        <div className="con-input">
+          <input placeholder="Password" type={!visible ? "password" : "text"} />
+          <i className="icon">
+            <Lock />
+          </i>
+          <i onClick={() => PasswordShow()}>
+            {!visible ? <Eyecut /> : <Eye />}
+          </i>
+          <div className="bg"></div>
+        </div>
+        <button className="btn">Log In</button>
+        <span>or</span>
+        <div className="afteror">
+          <a href="https://hacktoberfest.digitalocean.com" className="new">
+            Create an account
+          </a>
+          <div className="seperator"></div>
+          <a href="https://hacktoberfest.digitalocean.com" className="new">
+            Forgot password?
+          </a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
