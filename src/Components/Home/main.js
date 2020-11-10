16fdd984f8525e6ab8d356c6f66df7f771e47256
iamakkar <52326103+iamakkar@users.noreply.github.com>
2020-11-10 11:41:21 +0530

    .

--- src/Components/Home/main.js
+++ src/Components/Home/main.js
@@ -11,7 +11,7 @@ function App() {
   const history = useHistory();
   document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
+    M.Carousel.init(elems, {
       fullWidth: false,
       indicators: true
     });
@@ -97,17 +97,17 @@ function App() {
       <div class="container-fluid  blue lighten-2">
         <div className="row section2_upperRow">
           <div className="col s12">
-            <h1 className="center-align" data-aos="fade-up">Our Reach </h1>
+            <h1 className="center-align" data-aos="fade-up">Sign Up as </h1>
           </div>
         </div>
         <div class="row section2_middleRow_edit">
           <div class="col s12 m6 center-align" data-aos="fade-in">
-            <img src={require("../Home/influencer.png")} className="influencer_brand_logo" />
+            <img src={require("../Home/influencer.png")} className="influencer_brand_logo" alt={"Error-404"} />
             <button class="btnhome" onClick={() => history.push('/createaccount1')}>I'M INFLUENCER</button>
             <br></br>
           </div>
           <div class="col s12 m6 center-align" data-aos="fade-in">
-            <img src={require("../Home/brand.png")} className="influencer_brand_logo" />
+            <img src={require("../Home/brand.png")} className="influencer_brand_logo" alt={"Error-404"} />
             <button class="btnhome">I'M BRAND</button>
           </div>
         </div>
@@ -215,7 +215,7 @@ function App() {
 
               <div class="carousel-item card blue darken-4 reviewCard z-depth-5" href="#one!">
                 <div class="card-content white-text">
-                  <span class="card-title"><img src={require("../Home/avatar_male.png")} className="reviewLogo"/></span>
+                  <span class="card-title"><img src={require("../Home/avatar_male.png")} className="reviewLogo" alt={"Error-404"} /></span>
                   <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
                 
@@ -223,7 +223,7 @@ function App() {
               </div>
               <div class="carousel-item card blue darken-4 reviewCard" href="#two!">
                 <div class="card-content white-text">
-                  <span class="card-title"><img src={require("../Home/avatar_male.png")} className="reviewLogo"/></span>
+                  <span class="card-title"><img src={require("../Home/avatar_male.png")} className="reviewLogo" alt={"Error-404"} /></span>
                   <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
                 
@@ -231,7 +231,7 @@ function App() {
               </div>
               <div class="carousel-item card blue darken-4 reviewCard" href="#three!">
                 <div class="card-content white-text">
-                  <span class="card-title"><img src={require("../Home/avatar_male.png")} className="reviewLogo"/></span>
+                  <span class="card-title"><img src={require("../Home/avatar_male.png")} className="reviewLogo" alt={"Error-404"} /></span>
                   <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
                 
