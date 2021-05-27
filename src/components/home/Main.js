53ca106eecfe73bc7941f745f9c40a8936e439ec
iamakkar <52326103+iamakkar@users.noreply.github.com>
2021-05-27 16:50:13 +0530

    content

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -40,30 +40,29 @@ function App(props) {
         </Parallax>
         <h2 className="home-abtus" >About Levance:</h2>
         <div className="home-abtus-und" ></div>
-        <p className="home-abtus-txt1">Levance is one of the most reliable and cost-efficient influencer marketing agency in India.</p>
-        <p className="home-abtus-txt2">We at Levance are here to fetch the brands the right set of influencers. With our team of diligent marketers and influencers with high engagement rates, we tend to deliver the best RoI.</p>
-        <p className="home-abtus-txt2">Our Motto is:</p>
-        <p className="home-abtus-txt3">"Grow Good, Evolve Better"</p>
-        <button className="home-abtus-btn" >Contact Us →</button>
+        <p className="home-abtus-txt1">Levance is a peculiar influencer marketing platform that works with nano and micro influencers.</p>
+        <p className="home-abtus-txt2">We provide the most reliable and cost-effective influencer marketing services. We help brands gaining a lot more eyeballs on their products by connecting them with the best set of social media influencers, and eventually, attaining high customer attention. With our team of diligent marketers and influencers with high engagement rates, we tend to deliver the best ROI.</p>
+        <p className="home-abtus-txt3">"Grow Good, Evolve Better"</p><br/>
+        {/* <button className="home-abtus-btn" >Contact Us →</button> */}
         <div className="home-brand" >
-            <h2 className="home-brand-hd" >Are You a Brand?</h2>
+            <h2 className="home-brand-hd" >How it Works for Brands</h2> 
             <div className="home-brand-und" ></div>
             <div className="home-brand-pr">
-                <Card num="1" head="Campaign Brief" des="Tell us about your brand objectives, campaign expectations, and desired results. These may include the product, content type, etc." />
-                <Card num="2" head="Creating a Campaign" des="Our team will find the best set of influencers for the campaign and will send a sample list for you to approve. We'll also design the whole campaign." />
-                <Card num="3" head="Results & Chill" des="Our creators build innovative content to showcase your brand on social media. We'll send you complete campaign metrics and analytics." />
+                <Card num="1" head="Campaign Brief" des="Tell us about your campaign objectives, expectations, and your product. These may also include the influencer's niche, content type etc. Receive a custom sample list of influencers for your product, approve the list and proceed further." />
+                <Card num="2" head="Creating a Campaign" des="Our team will design the most suitable and cost-effective campaign with the best set of influencers for your product/service. We will train the influencers according to your requirements and regular follow-ups will be done throughout the campaign." />
+                <Card num="3" head="Results & Chill" des="Sit back and enjoy the ever-engaging content made by our creators to showcase your brand on social media. Once all the content goes live, we'll send you complete campaign metrics and analytics along with all content links." />
             </div>
-            <button className="home-brand-btn" onClick={() => props.history.push("/brand")} >Know More →</button>
+            <button className="home-brand-btn" onClick={() => props.history.push("/brand")} >Register Now →</button>
         </div>
         <div className="home-brand" style={{marginTop: 0, backgroundColor: '#58d3e6'}} >
-            <h2 className="home-brand-hd" style={{color: '#3d3d3d'}}>Are You an Influencer?</h2>
+            <h2 className="home-brand-hd" style={{color: '#3d3d3d'}}>How it Works for Influencers</h2>
             <div className="home-infl-und" ></div>
             <div className="home-brand-pr" style={{backgroundColor: '#58d3e6'}} >
                 <CardI num="1" head="Campaign Brief" des="Tell us about your brand objectives, campaign expectations, and desired results. These may include the product, content type, etc." />
                 <CardI num="2" head="Creating a Campaign" des="Our team will find the best set of influencers for the campaign and will send a sample list for you to approve. We'll also design the whole campaign." />
                 <CardI num="3" head="Results & Chill" des="Our creators build innovative content to showcase your brand on social media. We'll send you complete campaign metrics and analytics." />
             </div>
-            <button className="home-brand-btn" onClick={() => props.history.push("/influencer")} >Know More →</button>
+            <button className="home-brand-btn" onClick={() => props.history.push("/influencer")} >Register Now →</button>
         </div>
         <div className="home-team">
             <div className="home-team-chld" >
--- src/components/influencer/Main.js
+++ src/components/influencer/Main.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Main.css'
 import {Parallax, Background} from 'react-parallax'
 
 function App() {
+    const [base, setBase] = useState(false);
+    const [detail, setDetail] = useState({
+        FullName: "",
+        Email: "",
+        Phone: "",
+        Gender: "",
+        DOB: "",
+        State: "",
+        City: "",
+        Niche: "",
+        Instagram: "",
+        Youtube: ""
+    })
+
+    const handleNext = () => {
+        setBase(true)
+    }
+
+    const handleBack = () => {
+        setBase(false)
+    }
+
+    const handleSubmit = () => {
+        console.log("l")
+    }
+
+    function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const FullName = (e) => {
+        setDetail({...detail, FullName: e.target.value})
+        console.log(detail)
+    } 
+
+
+
     return (
         <>
         {/* <img src="/assets/brand.png" /> */}
@@ -25,65 +63,96 @@ function App() {
         <br />
       </div> 
     </Parallax>
-    <div className="infl-form-cont">
-    <div className="infl-form" >
-        <h2>Fill the FormI</h2><br/>
-        <div className="infl-form-cmpt" >
-            <span>Full Name<sup style={{color: 'red'}} >*</sup></span>
-            <input type="text" />
-        </div>
-        <br/>
-        <div className="infl-form-cmpt" >
-            <span>Email-Id<sup style={{color: 'red'}} >*</sup></span>
-            <input type="email" />
-        </div>
-        <br/>
-        <div className="infl-form-cmpt" >
-            <span>Phone<sup style={{color: 'red'}} >*</sup></span><br/>
-            <div>
-            <input id="countrycode" type="tel" defaultValue="+91" />{'-'}
-            <input id="phone" type="tel" />
+    {base ? <>
+        <div className="infl-form-cont" id="social">
+            <div className="infl-form" id="social" >
+                <h2>Let's Connect Socially</h2><br/>
+                <div className="infl-form-cmpt" >
+                <span>Niche<sup style={{color: 'red'}} >*</sup></span>
+                <input id="niche" type="text" value={detail.Niche} />
+                </div>
+                <br/>
+                <div className="infl-form-cmpt" >
+                <span>Instagram Handle</span>
+                <input id="instagram" type="text" />
+                </div>
+                <br/>
+                <div className="infl-form-cmpt" >
+                    <span>Youtube Handle</span>
+                    <input id="youtube" type="text" />
+                </div>
+                <br/>
+                <div className="infl-form-sub"  >
+                <button style={{marginRight: 5}} className="infl-form-btn" onClick={() => handleBack()} >← Back</button>
+                <button className="brnd-form-btn" onClick={() => handleSubmit()} >Submit</button>
+                </div>
+                <br/>
             </div>
-        </div>
-        <br/>
-    </div>
-    <div className="infl-form">
-    <h2 id="blank" >&#8203;</h2><br/>
-    <div className="infl-form-cmpt" >
-            <span>Gender<sup style={{color: 'red'}} >*</sup></span>
-            <span>Male</span><input type="radio" id="male" name="gender" value="male"/>
-            
-            <span>Female</span><input type="radio" id="female" name="gender" value="female"/>
-            
-            <span>Other</span><input type="radio" id="other" name="gender" value="other"/>
-    </div>
-    <br/>
-    <div className="infl-form-cmpt" >
-        <span>Date of Birth<sup style={{color: 'red'}} >*</sup></span>
-        <input id="date" type="date" />
-    </div>
-        <br/>
-        <div className="infl-form-cmpt" >
-            <span>State<sup style={{color: 'red'}} >*</sup></span>
-            <select defaultValue="A" id="budget" >
-                <option value="A" >A</option>
-                <option value="B" >B</option>
-                <option value="c" >c</option>
-            </select>
-        </div>
-        <br/>
-        <div className="infl-form-cmpt" >
-            <span>City<sup style={{color: 'red'}} >*</sup></span>
-            <input type="text" />
-        </div>
-        <br/>
-    </div>
-    </div>
-    <br/>
-    <div className="infl-form-sub"  >
-        <button className="infl-form-btn" >Next →</button>
-        </div>
-        <br/>
+            </div><br/>
+            </> : 
+            <>
+            <div className="infl-form-cont">
+            <div className="infl-form" >
+                <h2>Fill the FormI</h2><br/>
+                <div className="infl-form-cmpt" >
+                    <span>Full Name<sup style={{color: 'red'}} >*</sup></span>
+                    <input value={detail.FullName} type="text" onChange={(e) => FullName(e)} />
+                </div>
+                <br/>
+                <div className="infl-form-cmpt" >
+                    <span>Email-Id<sup style={{color: 'red'}} >*</sup></span>
+                    <input type="email" />
+                </div>
+                <br/>
+                <div className="infl-form-cmpt" >
+                    <span>Phone<sup style={{color: 'red'}} >*</sup></span><br/>
+                    <div>
+                    <input id="countrycode" type="tel" defaultValue="+91" />{'-'}
+                    <input id="phone" type="tel" />
+                    </div>
+                </div>
+                <br/>
+            </div>
+            <div className="infl-form">
+            <h2 id="blank" >&#8203;</h2><br/>
+            <div className="infl-form-cmpt" >
+                    <span>Gender<sup style={{color: 'red'}} >*</sup></span>
+                    <span>Male</span><input type="radio" id="male" name="gender" value="male"/>
+                    
+                    <span>Female</span><input type="radio" id="female" name="gender" value="female"/>
+                    
+                    <span>Other</span><input type="radio" id="other" name="gender" value="other"/>
+            </div>
+            <br/>
+            <div className="infl-form-cmpt" >
+                <span>Date of Birth<sup style={{color: 'red'}} >*</sup></span>
+                <input id="date" type="date" />
+            </div>
+                <br/>
+                <div className="infl-form-cmpt" >
+                    <span>State<sup style={{color: 'red'}} >*</sup></span>
+                    <select defaultValue="A" id="budget" >
+                        <option value="A" >A</option>
+                        <option value="B" >B</option>
+                        <option value="c" >c</option>
+                    </select>
+                </div>
+                <br/>
+                <div className="infl-form-cmpt" >
+                    <span>City<sup style={{color: 'red'}} >*</sup></span>
+                    <input type="text" />
+                </div>
+                <br/>
+            </div>
+            </div>
+            <br/>
+            <div className="infl-form-sub"  >
+                <button className="infl-form-btn" onClick={() => handleNext()} >Next →</button>
+            </div>
+            <br/>
+            </>
+            }
+    
         </>
     )
 }
