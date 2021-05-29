4606c67e521e034f3047d29ad3fcaabf001ac388
iamakkar <52326103+iamakkar@users.noreply.github.com>
2021-05-30 00:53:08 +0530

    final

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Main.css'
 import { Parallax } from 'react-parallax';
-import { SocialIcon } from 'react-social-icons'; 
 import {withRouter} from 'react-router-dom'
 
 function App(props) {
@@ -51,29 +50,39 @@ function App(props) {
                 <Card num="2" head="Creating a Campaign" des="Our team will design the most suitable and cost-effective campaign with the best set of influencers for your product/service. We will train the influencers according to your requirements and regular follow-ups will be done throughout the campaign." />
                 <Card num="3" head="Results & Chill" des="Sit back and enjoy the ever-engaging content made by our creators to showcase your brand on social media. Once all the content goes live, we'll send you complete campaign metrics and analytics along with all content links." />
             </div>
-            <button className="home-brand-btn" onClick={() => props.history.push("/brand")} >Register Now →</button>
+            <button className="home-brand-btn" onClick={() => props.history.push("/brand")} >Get a Quote →</button>
         </div>
         <div className="home-brand" style={{marginTop: 0, backgroundColor: '#58d3e6'}} >
             <h2 className="home-brand-hd" style={{color: '#3d3d3d'}}>How it Works for Influencers</h2>
             <div className="home-infl-und" ></div>
             <div className="home-brand-pr" style={{backgroundColor: '#58d3e6'}} >
-                <CardI num="1" head="Campaign Brief" des="Tell us about your brand objectives, campaign expectations, and desired results. These may include the product, content type, etc." />
-                <CardI num="2" head="Creating a Campaign" des="Our team will find the best set of influencers for the campaign and will send a sample list for you to approve. We'll also design the whole campaign." />
-                <CardI num="3" head="Results & Chill" des="Our creators build innovative content to showcase your brand on social media. We'll send you complete campaign metrics and analytics." />
+                <CardI num="1" head="Join the Community" des={`Click the "Register Now" button below to get yourself registered with us in less than 2 minutes. Fill in your basic details, your social media handles and choose the categories that suit best your content.`} />
+                <CardI num="2" head="Participate in Campaigns" des="Follow us on Instagram, where we post details about all the ongoing campaigns, participate in the one's you like. Else, our campaign managers are always there to reach out to you with all the suitable campaigns." />
+                <CardI num="3" head="Create for Credit" des="Blow the trumpet with your jaw-dropping content and make your audience believe in the brand you are promoting. Finally, get paid on successfully campaign completion and wait for the next one!" />
             </div>
             <button className="home-brand-btn" onClick={() => props.history.push("/influencer")} >Register Now →</button>
         </div>
-        <div className="home-team">
-            <div className="home-team-chld" >
-                <img src='/assets/me.jpeg' alt='err' />
-                <h4>Deepansh Makkar</h4>
-                <div>
-                    <SocialIcon className="socialicon" network="linkedin" url="https://www.linkedin.com/in/deepansh-makkar-175012197/" />
-                    <SocialIcon className="socialicon" network="instagram" url="https://www.instagram.com/iamakkar" />
-                    <SocialIcon className="socialicon" network="email" url="mailto:makkardeepansh@gmail.com" />
-                </div>
-                <p>Pranay previously co-founded LetsIntern.com (leading platform for college students to find internships in India) from 2010 until it's acquisition in 2016. From starting volunteering at the age of 14, to leading global teams at AIESEC through college, Pranay has always been most passionate about solving real world & business problems at scale. He loves talking shop (dhanda), new ideas & technologies, advertising, music, travel & everything cool.</p>
+        <div className="home-why-top" >
+            <h1 className="home-why-head">Why Levance?</h1>
+            <div className="home-infl-und" ></div>
+        <div className="home-why">
+            <div className="home-why-chld">
+                <img src="/assets/c1.png" alt="err" />
+                <p>Connect with hundreds of bona-fied influencers accross various platforms</p>
             </div>
+            <div className="home-why-chld">
+                <img src="/assets/c2.png" alt="err" />
+                <p>Get high ROI And most scalable campaigns</p>
+            </div>
+            <div className="home-why-chld">
+                <img src="/assets/c3.png" alt="err" />
+                <p>Get insightfull, data driven campaigns</p>
+            </div>
+            <div className="home-why-chld">
+                <img src="/assets/c4.png" alt="err" />
+                <p>Make your brand name to reach right target audience with us</p>
+            </div>
+        </div>
         </div>
          </>
     )
